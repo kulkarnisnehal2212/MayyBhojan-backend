dd264d2b8f0122da6a7dac8bc07369edf93b1375
kulkarnisnehal947 <kulkarnisnehal2212@gmail.com>
2026-03-12 17:16:57 +0530

    Initial Spring Boot backend structure

---
+++ src/main/java/com/example/maybhojan_backend/MaybhojanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.maybhojan_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaybhojanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaybhojanBackendApplication.class, args);
+	}
+
+}
